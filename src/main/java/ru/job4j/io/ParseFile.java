de9da632ef0757d008260f8406d422e03ada4e44
AkiBoba <lettervl@ya.ru>
2022-03-03 16:58:25 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -1,8 +1,11 @@
 package ru.job4j.io;
 
+import net.jcip.annotations.ThreadSafe;
+
 import java.io.*;
 import java.util.function.Predicate;
 
+@ThreadSafe
 public final class ParseFile {
     private final File file;
 
@@ -10,11 +13,11 @@ public final class ParseFile {
         this.file = file;
     }
 
-    public String getContent(Predicate<Character> filter) {
+    public synchronized String getContent(Predicate<Character> filter) {
         StringBuilder output = new StringBuilder();
         try (InputStream i = new FileInputStream(file)) {
             int data;
-            while ((data = i.read()) > 0) {
+            while ((data = i.read()) != -1) {
                 if (filter.test((char) data)) {
                     output.append((char) data);
                 }
