63516df43cc73c42491e57a0704f176b49c36a49
AkiBoba <lettervl@ya.ru>
2022-03-03 13:05:02 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        try (InputStream i = new FileInputStream(file)) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+}
