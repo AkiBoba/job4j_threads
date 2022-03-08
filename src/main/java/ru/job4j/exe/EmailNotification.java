5a618e4aafe0cd36e9cab32da8ea8b68f485db88
Vladimir Likhachev <lettervl@ya.ru>
2022-03-08 17:08:30 +0300

    2. ExecutorService рассылка почты. [#63097]

--- src/main/java/ru/job4j/exe/EmailNotification.java
+++ src/main/java/ru/job4j/exe/EmailNotification.java
@@ -5,19 +5,17 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    static ExecutorService pool = Executors.newFixedThreadPool(
+    private final ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors()
     );
-    String subject;
-    String body;
 
     public void notification(List<User> users) {
-        pool.submit(() -> users.forEach(user -> emailTo(user)));
+        pool.submit(() -> users.forEach(this::emailTo));
     }
 
     public void emailTo(User user) {
-        subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
-        body = String.format("Add a new event to %s", user.getUsername());
+        String subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getUsername());
         send(subject, body, user.getEmail());
     }
 
@@ -33,5 +31,12 @@ public class EmailNotification {
 
     public void send(String subject, String body, String email) {
 
+    }
+
+    public static void main(String[] args) {
+        EmailNotification emailNotification = new EmailNotification();
+        User user = new User("Petr", "petr@mail.com");
+        emailNotification.emailTo(user);
+        emailNotification.close();
     }
 }
